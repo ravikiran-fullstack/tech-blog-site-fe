61117e4e43ca52b414b286c59bc51ab4662ff1e5
Ravikiran Yadava <ravikiransjce.jobs@gmail.com>
2021-03-17 09:50:30 +0530

    Added dropdown menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import './Header.css';
 
 const Header = () => {
@@ -9,12 +8,33 @@ const Header = () => {
     window.location.assign(`/search?search=${searchString}`);
   };
 
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+
   const [searchString, setSearchString] = useState();
-  const [active, setActive] = useState(false);
+  
   const [showProfileOptions, setShowProfileOptions] = useState(false);
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      if(dropdownRef.current !== null && !dropdownRef.current.contains(e.target)){
+        console.log(e);
+        setIsActive(!isActive);
+      }
+    }
+
+    if(isActive){
+      window.addEventListener('click', pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener('click', pageClickEvent);
+    }
+
+  }, [isActive]);
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-light"
+      className="navbar navbar-expand-lg navbar-light menuContainer"
       style={{ background: "transparent" }}
     >
       <Link className="navbar-brand" to="/home">
@@ -28,12 +48,12 @@ const Header = () => {
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
-        onClick={() => setActive(!active)}
+        onClick={() => setIsActive(!isActive)}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className={`collapse navbar-collapse ${active && "menuOpen"}`} id="navbarSupportedContent">
+      <div ref={dropdownRef} className={`navbar-collapse menu ${isActive? "active" : "inactive"}`} id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <Link className="nav-link" to="/home">
@@ -87,13 +107,12 @@ const Header = () => {
           >
             Search
           </button>
+          <button className="btn btn-info my-2 my-sm-0 ml-2">login</button>
           {/*  */}
-          <div className="dropdown" onClick={() => setShowProfileOptions(!showProfileOptions)}>
+          {/* <div>
             <img className="customAvatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
-            <div className={`dropdown-content ${showProfileOptions && "dropdown-content-active"}`}>
               <button className="btn btn-info my-2 my-sm-0 ml-2">login</button>
-            </div>
-          </div>
+          </div> */}
         </form>
       </div>
     </nav>
